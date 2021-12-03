e3e89ff52065257ee4ca4c47da19d3ff711283da
Dave Bitter <dbitter@anwb.nl>
2021-12-03 15:27:28 +0100

    feat(app): Build demo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,100 +1,59 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData, json, Link } from "remix";
+import type { LoaderFunction } from "remix";
+import { useLoaderData, Link, useCatch } from "remix";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
 
-type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
+type LoaderData = { randomJoke: Joke };
+
+export const loader: LoaderFunction = async () => {
+    const count = await db.joke.count();
+    const randomRowNumber = Math.floor(Math.random() * count);
+    const [randomJoke] = await db.joke.findMany({
+        take: 1,
+        skip: randomRowNumber
+    });
+    if (!randomJoke) {
+        throw new Response("No random joke found", {
+            status: 404
+        });
+    }
+    const data: LoaderData = { randomJoke };
+    return data;
 };
 
-// Loaders provide data to components and are only ever called on the server, so
-// you can connect to a database or run any server side code you want right next
-// to the component that renders it.
-// https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = () => {
-  let data: IndexData = {
-    resources: [
-      {
-        name: "Remix Docs",
-        url: "https://remix.run/docs"
-      },
-      {
-        name: "React Router Docs",
-        url: "https://reactrouter.com/docs"
-      },
-      {
-        name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d"
-      }
-    ],
-    demos: [
-      {
-        to: "demos/actions",
-        name: "Actions"
-      },
-      {
-        to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading"
-      },
-      {
-        to: "demos/params",
-        name: "URL Params and Error Boundaries"
-      }
-    ]
-  };
+export default function JokesIndexRoute() {
+    const data = useLoaderData<LoaderData>();
 
-  // https://remix.run/api/remix#json
-  return json(data);
-};
+    return (
+        <div>
+            <p>Here's a random joke:</p>
+            <p>{data.randomJoke.content}</p>
+            <Link to={data.randomJoke.id}>
+                "{data.randomJoke.name}" Permalink
+            </Link>
+        </div>
+    );
+}
 
-// https://remix.run/api/conventions#meta
-export let meta: MetaFunction = () => {
-  return {
-    title: "Remix Starter",
-    description: "Welcome to remix!"
-  };
-};
+export function CatchBoundary() {
+    const caught = useCatch();
 
-// https://remix.run/guides/routing#index-routes
-export default function Index() {
-  let data = useLoaderData<IndexData>();
+    if (caught.status === 404) {
+        return (
+            <div className="error-container">
+                There are no jokes to display.
+            </div>
+        );
+    }
+    throw new Error(
+        `Unexpected caught response with status: ${caught.status}`
+    );
+}
 
-  return (
-    <div className="remix__page">
-      <main>
-        <h2>Welcome to Remix!</h2>
-        <p>We're stoked that you're here. 🥳</p>
-        <p>
-          Feel free to take a look around the code to see how Remix does things,
-          it might be a bit different than what you’re used to. When you're
-          ready to dive deeper, we've got plenty of resources to get you
-          up-and-running quickly.
-        </p>
-        <p>
-          Check out all the demos in this starter, and then just delete the{" "}
-          <code>app/routes/demos</code> and <code>app/styles/demos</code>{" "}
-          folders when you're ready to turn this into your next project.
-        </p>
-      </main>
-      <aside>
-        <h2>Demos In This App</h2>
-        <ul>
-          {data.demos.map(demo => (
-            <li key={demo.to} className="remix__page__resource">
-              <Link to={demo.to} prefetch="intent">
-                {demo.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <h2>Resources</h2>
-        <ul>
-          {data.resources.map(resource => (
-            <li key={resource.url} className="remix__page__resource">
-              <a href={resource.url}>{resource.name}</a>
-            </li>
-          ))}
-        </ul>
-      </aside>
-    </div>
-  );
+export function ErrorBoundary() {
+    return (
+        <div className="error-container">
+            I did a whoopsies.
+        </div>
+    );
 }
